5538a6f04bd1ccfed32e0c0f8a974a4baf7cb649
ParkerCatz <tksm.webdesign09@gmail.com>
2021-07-06 21:30:12 +0900

    feat: useState

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,24 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+import { useState } from 'react'
+function CompA() {
+  return <h1>CompA</h1>
 }
 
+function Home() {
+  const [value, setValue] = useState(10)
+  const incrementValue = () => {
+    setValue(value + 1)
+  }
+  const decrementValue = () => {
+    setValue(value - 1)
+  }
+  return (
+    <>
+      current Value: <h1>{value}</h1>
+      <button onClick={incrementValue}>+</button>
+      <button onClick={decrementValue}>-</button>
+      <CompA />
+    </>
+  )
+}
+
+export default Home
