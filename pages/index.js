4cfcf5cbe2122754aa2f7a2e1adaa7246d7c9772
ParkerCatz <tksm.webdesign09@gmail.com>
2021-07-11 23:11:39 +0900

    feat: Footerコンポーネント作成

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Navbar from '../components/Navbar'
 import ResourceHighlight from '../components/ResourceHighlight'
 import NewsLetter from '../components/NewsLetter'
 import ResourceList from '../components/ResourceList'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
@@ -10,18 +11,7 @@ function Home() {
       <ResourceHighlight />
       <NewsLetter />
       <ResourceList />
-
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <p>
-            <strong>Bulma - Blog theme</strong> by{' '}
-            <a href="https://gonzalojs.com">Gonzalo Gutierrez</a>. Based on the{' '}
-            <a href="http://jigsaw-blog-staging.tighten.co/">jigsaw-blog</a>.
-            The source code is licensed
-            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </>
   )
 }
