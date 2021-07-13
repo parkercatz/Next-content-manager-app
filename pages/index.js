cb6cea11befd53f0f8cb7c4882fb65bf479f1e3f
ParkerCatz <tksm.webdesign09@gmail.com>
2021-07-13 23:02:41 +0900

    feat: data.jsの追加

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,15 @@ import NewsLetter from 'components/NewsLetter'
 import ResourceList from 'components/ResourceList'
 import Footer from 'components/Footer'
 
+import { resources } from 'pages/api/data'
+
 function Home() {
   return (
     <Layout>
       <ResourceHighlight />
       <NewsLetter />
       <ResourceList />
+      {JSON.stringify(resources)}
       <Footer />
     </Layout>
   )
