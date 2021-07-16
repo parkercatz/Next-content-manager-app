4170f2511f565f1bc912c23d31225bd6b9e56281
ParkerCatz <tksm.webdesign09@gmail.com>
2021-07-16 22:39:41 +0900

    feat: Resource new page

---
+++ pages/resources/new.js
@@ -0,0 +1,11 @@
+import Layout from 'components/Layout'
+
+const ResourceCreate = () => {
+  return (
+    <Layout>
+      <div>I am Resource Create page</div>
+    </Layout>
+  )
+}
+
+export default ResourceCreate
