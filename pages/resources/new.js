5f628b51109121b6673c1c2c7c798cf22f2803bb
ParkerCatz <tksm.webdesign09@gmail.com>
2021-07-17 12:58:04 +0900

    feat: resource form

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -3,7 +3,27 @@ import Layout from 'components/Layout'
 const ResourceCreate = () => {
   return (
     <Layout>
-      <div>I am Resource Create page</div>
+      <div className="container">
+        <div className="columns">
+          <div className="column is-8 is-offset-2">
+            <div className="resource-form">
+              <h1 className="title">Add New Respurce</h1>
+              <form action="">
+                <div className="field">
+                  <label className="label">Name</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Text input"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
