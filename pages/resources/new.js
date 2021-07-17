ae48fe9e4f893212392cbf224c59db244f9c7e96
ParkerCatz <tksm.webdesign09@gmail.com>
2021-07-17 13:14:17 +0900

    feat: more inputs

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -15,8 +15,60 @@ const ResourceCreate = () => {
                     <input
                       className="input"
                       type="text"
-                      placeholder="Text input"
+                      placeholder="Learn Next JS and Sanity IO"
                     />
+                  </div>
+                </div>
+                <div class="field">
+                  <label className="label">Description</label>
+                  <div class="control">
+                    <textarea
+                      class="textarea"
+                      placeholder="Textarea"
+                      placeholder="Learn these technologies because they are very popular and enable better SEO"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Link</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="https://academy.eincode.com"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Priority</label>
+                  <div className="control">
+                    <div className="select">
+                      <select>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">TimeToFinish</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="60 (Time is in minutes)"
+                    />
+                  </div>
+                  <div className="field is-grouped">
+                    <div className="control">
+                      <button className="button is-link">Submit</button>
+                    </div>
+                    <div className="control">
+                      <button className="button is-link is-light">
+                        Cancel
+                      </button>
+                    </div>
                   </div>
                 </div>
               </form>
