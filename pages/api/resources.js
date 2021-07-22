ce23a189feb29356a9077086e829d5ac125a9fff
ParkerCatz <tksm.webdesign09@gmail.com>
2021-07-22 21:59:55 +0900

    feat: attach data in the request

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,5 +1,11 @@
 export default async function(req, res) {
-  const dataRes = await fetch('http://localhost:3001/api/resources')
-  const data = await dataRes.json()
-  res.send(data)
+  if (req.method === 'GET') {
+    const dataRes = await fetch('http://localhost:3001/api/resources')
+    const data = await dataRes.json()
+    res.send(data)
+  }
+  if (req.method === 'POST') {
+    console.log(req.body)
+    return res.send('Data has been received')
+  }
 }
