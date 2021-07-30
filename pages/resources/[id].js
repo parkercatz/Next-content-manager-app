91cfbb2903de48616caab603d200a050890b6465
ParkerCatz <tksm.webdesign09@gmail.com>
2021-07-30 23:14:51 +0900

    feat: Resource page fetch data

--- pages/resources/[id].js
+++ pages/resources/[id].js
@@ -1,5 +1,5 @@
 import Layout from 'components/Layout'
-const ResourceDetail = () => {
+const ResourceDetail = ({ resource }) => {
   return (
     <Layout>
       <section className="hero ">
@@ -9,9 +9,9 @@ const ResourceDetail = () => {
               <div className="columns">
                 <div className="column is-8 is-offset-2">
                   <div className="content is-medium">
-                    <h2 className="subtitle is-4">DATE HERE</h2>
-                    <h1 className="title">TITLE HERE</h1>
-                    <p>DESC HERE</p>
+                    <h2 className="subtitle is-4">{resource.createdAt}</h2>
+                    <h1 className="title">{resource.title}</h1>
+                    <p>{resource.description}</p>
                   </div>
                 </div>
               </div>
@@ -21,6 +21,18 @@ const ResourceDetail = () => {
       </section>
     </Layout>
   )
+}
+
+export async function getServerSideProps({ params }) {
+  const dataRes = await fetch(
+    `http://localhost:3001/api/resources/${params.id}`
+  )
+  const data = await dataRes.json()
+  return {
+    props: {
+      resource: data,
+    },
+  }
 }
 
 export default ResourceDetail
