26571276c9ca4107946e4cee7700e3cddbc8f0c1
ParkerCatz <tksm.webdesign09@gmail.com>
2021-08-02 22:06:43 +0900

    feat: Get static paths

--- pages/resources/[id].js
+++ pages/resources/[id].js
@@ -23,15 +23,23 @@ const ResourceDetail = ({ resource }) => {
   )
 }
 
-// ResourceDetail.getInitialProps = async ({ query }) => {
-//   const dataRes = await fetch(`http://localhost:3001/api/resources/${query.id}`)
-//   const data = await dataRes.json()
-//   return {
-//     resource: data,
-//   }
-// }
+export async function getStaticPaths() {
+  const resData = await fetch('http://localhost:3001/api/resources')
+  const data = await resData.json()
+  const paths = data.map((resource) => {
+    return {
+      params: { id: resource.id },
+    }
+  })
 
-export async function getServerSideProps({ params }) {
+  return {
+    paths,
+    // means that other routes should resolve into 404 page
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
   const dataRes = await fetch(
     `http://localhost:3001/api/resources/${params.id}`
   )
