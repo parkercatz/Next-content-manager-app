1f5c47b141b823f7286a61b6ee782f7ff8d08e34
ParkerCatz <tksm.webdesign09@gmail.com>
2021-08-08 22:17:05 +0900

    feat: Resource edit form

--- pages/resources/[id]/edit.js
+++ pages/resources/[id]/edit.js
@@ -1,7 +1,37 @@
 import Layout from 'components/Layout'
+import ResourceForm from 'components/ResourceForm'
+const ResourceEdit = ({ resource }) => {
+  const updateResource = (formData) => {
+    alert(JSON.stringify(formData))
+  }
 
-const ResourceEdit = () => {
-  return <Layout>I am edit page!</Layout>
+  return (
+    <Layout>
+      <div className="container">
+        <div className="columns">
+          <div className="column is-8 is-offset-2">
+            <ResourceForm
+              initialData={resource}
+              onFormSubmit={updateResource}
+            />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const dataRes = await fetch(
+    `http://localhost:3001/api/resources/${params.id}`
+  )
+  const data = await dataRes.json()
+
+  return {
+    props: {
+      resource: data,
+    },
+  }
 }
 
 export default ResourceEdit
